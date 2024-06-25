841075c0e399bc045f1f6db3709e499a5f13a09e
l-luz <lyraluz@yahoo.com.br>
2024-06-25 18:24:50 -0300

    Programa window refact
    Co-Authored-By: Maria Carolina Esteves <71904551+Mcesteves@users.noreply.github.com>
    Co-Authored-By: Luíza Camerini <100093016+luizafcamerini@users.noreply.github.com>

--- ProgramaWindow.py
+++ ProgramaWindow.py
@@ -1,5 +1,6 @@
 # import datetime
-from PySide6.QtWidgets import QDialog, QTableWidgetItem
+from PySide6.QtWidgets import QDialog, QTableWidget, QTableWidgetItem, QLabel, QScrollArea
+from bson import ObjectId
 from ui_ProgramaWindow import Ui_ProgramaWindow
 from banco import Banco
 from PySide6.QtCore import Signal, Slot, Qt
@@ -10,6 +11,8 @@ class ProgramaWindow(QDialog, Banco):
     exercicios = []
     linhas = 0
     current_item: QTableWidgetItem
+    tabelas_treino = list()
+
     def __init__(self, parent=None, exercicios = None, clientes = None):
         super().__init__(parent)
         self.exercicios = exercicios
@@ -23,14 +26,12 @@ class ProgramaWindow(QDialog, Banco):
         self.ui = Ui_ProgramaWindow()
         self.ui.setupUi(self)
         self.lista_clientes(clientes)
-        linhas = 1
-        self.ui.tableWidget.setRowCount(linhas)
 
         self.ui.cancel_button.clicked.connect(self.reject)
         self.ui.salvar_button.clicked.connect(self.accept)
         self.ui.add_treino.clicked.connect(self.adiciona_treino)
         self.ui.del_treino.clicked.connect(self.deleta_treino)
-
+        
     def lista_clientes(self, clientes):
         '''Popula combo box de clientes'''
         for cli in clientes:
@@ -51,44 +52,44 @@ class ProgramaWindow(QDialog, Banco):
     def adiciona_treino(self):
         dlg = CheckboxTableDialog(self.exercicios)
         if (dlg.exec() == QDialog.Accepted):
-            qtd_col = self.ui.tableWidget.columnCount()
-            qtd_lin = self.ui.tableWidget.rowCount()
-            print(qtd_lin)
-            if qtd_col == 0:
-                qtd_lin = 0
-                self.ui.tableWidget.setColumnCount(qtd_col+4)
-                self.ui.tableWidget.setHorizontalHeaderItem(0, QTableWidgetItem("Exercícios"))
-                self.ui.tableWidget.setHorizontalHeaderItem(1, QTableWidgetItem("Séries"))
-                self.ui.tableWidget.setHorizontalHeaderItem(2, QTableWidgetItem("Repetições"))
-                self.ui.tableWidget.setHorizontalHeaderItem(3, QTableWidgetItem("Peso"))
-            selecionados = dlg.get_selected_items()
-            linhas_selecionados = len(selecionados)
-            self.linhas = linhas_selecionados+qtd_lin+1
-            self.ui.tableWidget.setRowCount(self.linhas)
-            treino = QTableWidgetItem("Treino "+str(self.qtd_treinos))
-            treino.textAlignment =  Qt.AlignmentFlag.AlignHCenter
-            self.ui.tableWidget.setItem(qtd_lin, 0, treino)
-            self.ui.tableWidget.setSpan(qtd_lin, 0, 1, 4)
-            for row, item in enumerate(selecionados):
-                nome_item = QTableWidgetItem(item["nome"])
-                nome_item.setData(Qt.UserRole, item['_id'])
-                nome_item.setFlags(nome_item.flags() & ~Qt.ItemIsEditable)
-                self.ui.tableWidget.setItem(qtd_lin+1+row, 0, nome_item)
+            self.cria_tabela_treino(dlg.get_selected_items())
 
-                series_item = QTableWidgetItem("3")
-                series_item.setFlags(series_item.flags() | Qt.ItemIsEditable)
-                self.ui.tableWidget.setItem(qtd_lin+1+row, 1, series_item)
+    def cria_tabela_treino(self, exercicios):
+        prog_layout = self.ui.programa_layout
+        tb_treino = QTableWidget()
+        tb_treino.setColumnCount(4)
+        tb_treino.setHorizontalHeaderLabels(["Exercício", "Séries", "Repetições", "Peso"])
 
-                repeticao_item = QTableWidgetItem("15")
-                repeticao_item.setFlags(repeticao_item.flags() | Qt.ItemIsEditable)
-                self.ui.tableWidget.setItem(qtd_lin+1+row, 2, repeticao_item)
+        linhas_selecionados = len(exercicios)
+        tb_treino.setRowCount(linhas_selecionados)
+        self.qtd_treinos += 1
+        treino_name = "Treino "+str(self.qtd_treinos)
 
-                peso_item = QTableWidgetItem("10")
-                peso_item.setFlags(peso_item.flags() | Qt.ItemIsEditable)
-                self.ui.tableWidget.setItem(qtd_lin+1+row, 3, peso_item)
+        for row, item in enumerate(exercicios):
+            nome_item = QTableWidgetItem(item["nome"])
+            nome_item.setData(Qt.UserRole, item['_id'])
+            nome_item.setFlags(nome_item.flags() & ~Qt.ItemIsEditable)
+            tb_treino.setItem(row, 0, nome_item)
+
+            series_item = QTableWidgetItem("3")
+            series_item.setFlags(series_item.flags() | Qt.ItemIsEditable)
+            tb_treino.setItem(row, 1, series_item)
+
+            repeticao_item = QTableWidgetItem("15")
+            repeticao_item.setFlags(repeticao_item.flags() | Qt.ItemIsEditable)
+            tb_treino.setItem(row, 2, repeticao_item)
+
+            peso_item = QTableWidgetItem("10")
+            peso_item.setFlags(peso_item.flags() | Qt.ItemIsEditable)
+            tb_treino.setItem(row, 3, peso_item)
+
+        label = QLabel(treino_name)
+        prog_layout.addWidget(label)
+        prog_layout.addWidget(tb_treino)
+        self.tabelas_treino.append({"label": treino_name, "tab": tb_treino})
 
     def deleta_treino(self):
-        self.ui.tableWidget.removeColumn(self.ui.tableWidget.currentColumn())
+        # self.ui.tableWidget.removeColumn(self.ui.tableWidget.currentColumn())
         print('deleta t')
 
     # def adiciona_linha(self):
@@ -110,38 +111,27 @@ class ProgramaWindow(QDialog, Banco):
 
     def salva_treinos(self):
         treinos = list()
-        rowCount = self.ui.tableWidget.rowCount()
-        i = 0
-        info = dict()
-        exercicios = list()
-        while i < rowCount:
-            item = self.ui.tableWidget.item(i, 0)
-            if item.text().find("Treino") is not -1:
-                if len(exercicios) is not 0:
-                    info["exercicios"] = exercicios.copy()
-                    treinos.append(info.copy())
-                    exercicios.clear()
-                    info.clear()
-                info["name"] = item.text()
-            elif item is not None:
-                exercicio = dict()
-                exercicio["id"] = item.data(Qt.UserRole)
-                serie_item = self.ui.tableWidget.item(i, 1)
-                repeticao_item = self.ui.tableWidget.item(i, 2)
-                peso_item = self.ui.tableWidget.item(i, 3)
-                exercicio["series"] = serie_item.text()
-                exercicio["repeticoes"] = repeticao_item.text()
-                exercicio["peso"] = peso_item.text()
-                print(exercicio)
-                exercicios.append(exercicio)
-                print(exercicios)
-            i += 1
-        if len(exercicios) is not 0:
-            info["exercicios"] = exercicios.copy()
-            treinos.append(info.copy())
-            exercicios.clear()
-            info.clear()
-            print(treinos)
+        print("salva")
 
+        for lista in self.tabelas_treino:
+            info_treino = dict()
+            exercicios = list()
+            info_treino["nome"] = lista["label"]
+            treino = lista["tab"]
+            rowCount = treino.rowCount()
+            for i in range(rowCount):
+                item = treino.item(i, 0)
+                if item is not None and item.text():
+                    exercicio = dict()
+                    exercicio["id"] = item.data(Qt.UserRole)
+                    serie_item = treino.item(i, 1)
+                    repeticao_item = treino.item(i, 2)
+                    peso_item = treino.item(i, 3)
+                    exercicio["series"] = serie_item.text()
+                    exercicio["repeticoes"] = repeticao_item.text()
+                    exercicio["peso"] = peso_item.text()
+                    exercicios.append(exercicio)
+            info_treino["exercicios"] = exercicios.copy()
+            treinos.append(info_treino)
         insert_treinos = self.treino_col.insert_many(treinos)
         return list(insert_treinos.inserted_ids)
--- ui_ProgramaWindow.py
+++ ui_ProgramaWindow.py
@@ -15,78 +15,99 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QComboBox, QGridLayout,
-    QHeaderView, QLabel, QPushButton, QSizePolicy,
-    QSpacerItem, QTableWidget, QTableWidgetItem, QToolButton,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+    QPushButton, QScrollArea, QSizePolicy, QSpacerItem,
+    QToolButton, QVBoxLayout, QWidget)
 
 class Ui_ProgramaWindow(object):
     def setupUi(self, ProgramaWindow):
         if not ProgramaWindow.objectName():
             ProgramaWindow.setObjectName(u"ProgramaWindow")
         ProgramaWindow.resize(636, 497)
-        self.cancel_button = QPushButton(ProgramaWindow)
-        self.cancel_button.setObjectName(u"cancel_button")
-        self.cancel_button.setGeometry(QRect(440, 380, 80, 24))
-        self.salvar_button = QPushButton(ProgramaWindow)
-        self.salvar_button.setObjectName(u"salvar_button")
-        self.salvar_button.setGeometry(QRect(540, 380, 80, 24))
-        self.gridLayoutWidget_2 = QWidget(ProgramaWindow)
-        self.gridLayoutWidget_2.setObjectName(u"gridLayoutWidget_2")
-        self.gridLayoutWidget_2.setGeometry(QRect(10, 80, 621, 241))
-        self.gridLayout_2 = QGridLayout(self.gridLayoutWidget_2)
+        self.gridLayout_3 = QGridLayout(ProgramaWindow)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.cliente_c_box = QComboBox(ProgramaWindow)
+        self.cliente_c_box.setObjectName(u"cliente_c_box")
 
-        self.gridLayout_2.addItem(self.horizontalSpacer_2, 4, 2, 1, 3)
+        self.gridLayout_2.addWidget(self.cliente_c_box, 0, 1, 1, 2)
 
-        self.del_linha = QToolButton(self.gridLayoutWidget_2)
-        self.del_linha.setObjectName(u"del_linha")
+        self.cliente_label = QLabel(ProgramaWindow)
+        self.cliente_label.setObjectName(u"cliente_label")
 
-        self.gridLayout_2.addWidget(self.del_linha, 4, 1, 1, 1)
-
-        self.add_linha = QToolButton(self.gridLayoutWidget_2)
-        self.add_linha.setObjectName(u"add_linha")
-
-        self.gridLayout_2.addWidget(self.add_linha, 4, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.cliente_label, 0, 0, 1, 1)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout_2.addItem(self.verticalSpacer, 2, 5, 1, 1)
-
-        self.tableWidget = QTableWidget(self.gridLayoutWidget_2)
-        self.tableWidget.setObjectName(u"tableWidget")
-        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.tableWidget.setDragEnabled(True)
-        self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
-
-        self.gridLayout_2.addWidget(self.tableWidget, 1, 0, 2, 5)
+        self.gridLayout_2.addItem(self.verticalSpacer, 2, 4, 1, 1)
 
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.del_treino = QToolButton(self.gridLayoutWidget_2)
+        self.del_treino = QToolButton(ProgramaWindow)
         self.del_treino.setObjectName(u"del_treino")
 
         self.gridLayout.addWidget(self.del_treino, 0, 0, 1, 1)
 
-        self.add_treino = QToolButton(self.gridLayoutWidget_2)
+        self.add_treino = QToolButton(ProgramaWindow)
         self.add_treino.setObjectName(u"add_treino")
 
         self.gridLayout.addWidget(self.add_treino, 1, 0, 1, 1)
 
 
-        self.gridLayout_2.addLayout(self.gridLayout, 1, 5, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 1, 4, 1, 1)
 
-        self.cliente_label = QLabel(self.gridLayoutWidget_2)
-        self.cliente_label.setObjectName(u"cliente_label")
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout_2.addWidget(self.cliente_label, 0, 0, 1, 1)
+        self.gridLayout_2.addItem(self.verticalSpacer_2, 4, 4, 4, 1)
 
-        self.cliente_c_box = QComboBox(self.gridLayoutWidget_2)
-        self.cliente_c_box.setObjectName(u"cliente_c_box")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
-        self.gridLayout_2.addWidget(self.cliente_c_box, 0, 1, 1, 2)
+        self.gridLayout_2.addItem(self.horizontalSpacer, 8, 0, 1, 3)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout_2.addItem(self.horizontalSpacer_2, 4, 2, 1, 2)
+
+        self.add_linha = QToolButton(ProgramaWindow)
+        self.add_linha.setObjectName(u"add_linha")
+
+        self.gridLayout_2.addWidget(self.add_linha, 4, 0, 1, 1)
+
+        self.cancel_button = QPushButton(ProgramaWindow)
+        self.cancel_button.setObjectName(u"cancel_button")
+
+        self.gridLayout_2.addWidget(self.cancel_button, 8, 3, 1, 1)
+
+        self.salvar_button = QPushButton(ProgramaWindow)
+        self.salvar_button.setObjectName(u"salvar_button")
+
+        self.gridLayout_2.addWidget(self.salvar_button, 8, 4, 1, 1)
+
+        self.del_linha = QToolButton(ProgramaWindow)
+        self.del_linha.setObjectName(u"del_linha")
+
+        self.gridLayout_2.addWidget(self.del_linha, 4, 1, 1, 1)
+
+        self.scrollArea = QScrollArea(ProgramaWindow)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_2 = QWidget()
+        self.scrollAreaWidgetContents_2.setObjectName(u"scrollAreaWidgetContents_2")
+        self.scrollAreaWidgetContents_2.setGeometry(QRect(0, 0, 455, 369))
+        self.gridLayout_4 = QGridLayout(self.scrollAreaWidgetContents_2)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.programa_layout = QVBoxLayout()
+        self.programa_layout.setObjectName(u"programa_layout")
+
+        self.gridLayout_4.addLayout(self.programa_layout, 0, 0, 1, 1)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_2)
+
+        self.gridLayout_2.addWidget(self.scrollArea, 1, 0, 2, 4)
+
+
+        self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
 
 
         self.retranslateUi(ProgramaWindow)
@@ -96,12 +117,12 @@ class Ui_ProgramaWindow(object):
 
     def retranslateUi(self, ProgramaWindow):
         ProgramaWindow.setWindowTitle(QCoreApplication.translate("ProgramaWindow", u"MainWindow", None))
+        self.cliente_label.setText(QCoreApplication.translate("ProgramaWindow", u"Cliente", None))
+        self.del_treino.setText(QCoreApplication.translate("ProgramaWindow", u"Del Treino", None))
+        self.add_treino.setText(QCoreApplication.translate("ProgramaWindow", u"Add Treino", None))
+        self.add_linha.setText(QCoreApplication.translate("ProgramaWindow", u"Add Linha", None))
         self.cancel_button.setText(QCoreApplication.translate("ProgramaWindow", u"cancel", None))
         self.salvar_button.setText(QCoreApplication.translate("ProgramaWindow", u"Salvar", None))
         self.del_linha.setText(QCoreApplication.translate("ProgramaWindow", u"Del Linha", None))
-        self.add_linha.setText(QCoreApplication.translate("ProgramaWindow", u"Add Linha", None))
-        self.del_treino.setText(QCoreApplication.translate("ProgramaWindow", u"Del Treino", None))
-        self.add_treino.setText(QCoreApplication.translate("ProgramaWindow", u"Add Treino", None))
-        self.cliente_label.setText(QCoreApplication.translate("ProgramaWindow", u"Cliente", None))
     # retranslateUi
 
